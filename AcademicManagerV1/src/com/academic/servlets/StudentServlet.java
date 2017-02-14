c215af2c81a942d2492396afe9f74e204ae2cc98
maverstef <maverstef@gmail.com>
2017-02-14 19:24:46 +0200

    Removed details with the db

--- AcademicManagerV1/src/com/academic/servlets/StudentServlet.java
+++ AcademicManagerV1/src/com/academic/servlets/StudentServlet.java
@@ -16,8 +16,8 @@ import javax.sql.DataSource;
 public class StudentServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
  
-    @Resource(name = "jdbc/testDB")
-    DataSource ds;
+   
+   
  
     public StudentServlet() {
         super();
@@ -25,35 +25,7 @@ public class StudentServlet extends HttpServlet {
  
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            Connection con = ds.getConnection();
- 
-            Statement stmt = con.createStatement();
-            String query = "select * from students";
-            ResultSet rs = stmt.executeQuery(query);
- 
-            PrintWriter out = response.getWriter();
-            response.setContentType("text/html");
-            out.print("<center><h1>Student Details</h1></center>");
-            out.print("<html><body>");
-            out.print("<table border=\"1\" cellspacing=10 cellpadding=5>");
-            out.print("<tr><th>Student ID</th>");
-            out.print("<th>Student Name</th>");
-            out.print("<th>Student Surnanme</th>");
-            out.print("<th>Student Email</th></tr>");
- 
-            while (rs.next()) {
-                out.print("<tr>");
-                out.print("<td>" + rs.getInt("id") + "</td>");
-                out.print("<td>" + rs.getString("name") + "</td>");
-                out.print("<td>" + rs.getString("surname") + "</td>");
-                out.print("<td>" + rs.getString("email") + "</td>");
-                out.print("</tr>");
-            }
-            out.print("</table></body></html>");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+       
     }
  
     public void doPost(HttpServletRequest request, HttpServletResponse response)
