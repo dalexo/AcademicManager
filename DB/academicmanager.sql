7a388b6ce237ead5d4dec5188f906ec6d338bd31
cthanos <ctsthanos@gmail.com>
2017-04-05 13:24:49 +0300

    Changes on academicmanager.sql
    Changes on academicmanager.sql based on the suggestions made by G.Metaxas

--- DB/academicmanager.sql
+++ DB/academicmanager.sql
@@ -44,9 +44,7 @@ CREATE TABLE IF NOT EXISTS `person` (
   `phoneNumber` varchar(128) COLLATE utf8_bin NOT NULL,
   `address` varchar(128) COLLATE utf8_bin NOT NULL,
   `taxNumber` varchar(128) COLLATE utf8_bin NOT NULL,
-  `bankAccount` varchar(128) COLLATE utf8_bin NOT NULL,
-  `username` varchar(128) COLLATE utf8_bin NOT NULL,
-  `password` varchar(128) COLLATE utf8_bin NOT NULL,
+  `bankAccount` varchar(128) COLLATE utf8_bin NOT NULL
   PRIMARY KEY(personId)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -54,18 +52,18 @@ CREATE TABLE IF NOT EXISTS `person` (
 -- Άδειασμα δεδομένων του πίνακα `person`
 --
 
-INSERT INTO `person` (`personId`, `name`, `surname`, `type`, `dateOfBirth`, `sex`, `email`, `phoneNumber`, `address`, `taxNumber`, `bankAccount`, `username`, `password`) VALUES
-(1, 'Leonardo', 'Di Caprio', 'Student', '1972-11-25', 'Male', 'leodic@gmail.com', '6908585672', 'Sunset Boulevard 52', '2134567890', '', 'leodic', '12345678'),
-(3, 'Michael', 'Jackson', 'Student', '1965-09-19', 'Male', 'micjac@gmail.com', '6906969690', 'Kiriakidou 11', '2108156890', '', 'micjac', '12345678'),
-(4, 'George', 'Michael', 'Student', '1964-07-09', 'Male', 'geomic@gmail.com', '2108547852', 'Kolokotroni 55', '2103897345', '', 'geomic', '12345678'),
-(5, 'David', 'Bowie', 'Teacher', '1957-12-11', 'Male', 'dbowie@yahoo.com', '2106711512', 'Panepistimiou 13', '2101981123', 'GR69005300000098762', 'davbow', '12345678'),
-(6, 'Whitney', 'Hiouston', 'Secretary', '1966-05-31', 'Female', 'whitneyh@hotmail.com', '6908877651', 'Grammou 6', '2118890768', 'GR320058009011110000872', 'whitney', '12345678'),
-(7, 'Bruce', 'Lee', 'Teacher', '1951-01-03', 'Male', 'brulee@gmail.com', '4456789123', 'Patision 66', '6789005432', 'GB55879000001155662', 'brulee', '12345678'),
-(8, 'Mike', 'Tyson', 'Teacher', '1965-01-15', 'Male', 'miktys@gmail.com', '0090807050', 'Heaven 32', '9087654321', 'US77890110000530099112', 'miktys', '12345678'),
-(9, 'Madonna', 'Madonna', 'Student', '1967-11-13', 'Female', 'madonna@gmail.com', '6968674321', 'Sunset 88', '2324257610', '', 'madonna', '12345678'),
-(10, 'Tony', 'Montana', 'Teacher', '2048-02-13', 'Male', 'drugs@gmail.com', '6909001234', 'Cartel 9', '2134567890', 'GR0022375812093467', 'cocaine', '12345678'),
-(11, 'Luke', 'Skywalker', 'Admin', '1955-01-28', 'Male', 'jedi@gmail.com', '6932425261', 'Rebel Camp 4', '2341538753', 'GR238719094328734', 'admin', 'admin'),
-(12, 'Darth', 'Vader', 'Student', '1945-06-18', 'Male', 'sithlord@yahoo.com', '6974358721', 'Deathstar 10', '1452348790', '', 'sith', '12345678');
+INSERT INTO `person` (`personId`, `name`, `surname`, `type`, `dateOfBirth`, `sex`, `email`, `phoneNumber`, `address`, `taxNumber`, `bankAccount`) VALUES
+(1, 'Leonardo', 'Di Caprio', 'Student', '1972-11-25', 'Male', 'leodic@gmail.com', '6908585672', 'Sunset Boulevard 52', '2134567890', ''),
+(3, 'Michael', 'Jackson', 'Student', '1965-09-19', 'Male', 'micjac@gmail.com', '6906969690', 'Kiriakidou 11', '2108156890', ''),
+(4, 'George', 'Michael', 'Student', '1964-07-09', 'Male', 'geomic@gmail.com', '2108547852', 'Kolokotroni 55', '2103897345', ''),
+(5, 'David', 'Bowie', 'Teacher', '1957-12-11', 'Male', 'dbowie@yahoo.com', '2106711512', 'Panepistimiou 13', '2101981123', 'GR69005300000098762'),
+(6, 'Whitney', 'Hiouston', 'Secretary', '1966-05-31', 'Female', 'whitneyh@hotmail.com', '6908877651', 'Grammou 6', '2118890768', 'GR320058009011110000872'),
+(7, 'Bruce', 'Lee', 'Teacher', '1951-01-03', 'Male', 'brulee@gmail.com', '4456789123', 'Patision 66', '6789005432', 'GB55879000001155662'),
+(8, 'Mike', 'Tyson', 'Teacher', '1965-01-15', 'Male', 'miktys@gmail.com', '0090807050', 'Heaven 32', '9087654321', 'US77890110000530099112'),
+(9, 'Madonna', 'Madonna', 'Student', '1967-11-13', 'Female', 'madonna@gmail.com', '6968674321', 'Sunset 88', '2324257610', ''),
+(10, 'Tony', 'Montana', 'Teacher', '2048-02-13', 'Male', 'drugs@gmail.com', '6909001234', 'Cartel 9', '2134567890', 'GR0022375812093467'),
+(11, 'Luke', 'Skywalker', 'Admin', '1955-01-28', 'Male', 'jedi@gmail.com', '6932425261', 'Rebel Camp 4', '2341538753', 'GR238719094328734'),
+(12, 'Darth', 'Vader', 'Student', '1945-06-18', 'Male', 'sithlord@yahoo.com', '6974358721', 'Deathstar 10', '1452348790', '');
 
 -- --------------------------------------------------------
 
@@ -100,12 +98,6 @@ ALTER TABLE `course`
   ADD PRIMARY KEY (`courseId`);
 
 --
--- Ευρετήρια για πίνακα `person`
---
-/*ALTER TABLE `person`
-  ADD PRIMARY KEY (`personId`);*/
-
---
 -- Ευρετήρια για πίνακα `teaching`
 --
 ALTER TABLE `teaching`
@@ -128,7 +120,8 @@ ALTER TABLE `person`
 
 
 -- DROP the columns below as requested by G.Metaxas
-ALTER TABLE person DROP username,DROP password;
+ALTER TABLE person DROP IF EXISTS username;
+ALTER TABLE person DROP IF EXISTS password;
 
 
 -- Creation of table users
