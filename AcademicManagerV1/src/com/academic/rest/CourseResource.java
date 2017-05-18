ae1843724ac5db9c535284c96f9ddfc23eb3e55d
Michael Aligizakis <aligmic@gmail.com>
2017-05-18 20:21:23 +0300

    Add files via upload

--- AcademicManagerV1/src/com/academic/rest/CourseResource.java
+++ AcademicManagerV1/src/com/academic/rest/CourseResource.java
@@ -3,14 +3,22 @@ package com.academic.rest;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.academic.db.DAOFactory;
 import com.academic.model.Course;
 
 @Path("courses")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class CourseResource {
 
 	@GET
@@ -22,7 +30,16 @@ public class CourseResource {
 
 	@GET
 	@Path("{id}")
-	public Course course(@PathParam("id") int id) throws ClassNotFoundException, SQLException {
-		return DAOFactory.getInstance().getCourseDao().get(id);
+	public Response course(@PathParam("id") int id) throws ClassNotFoundException, SQLException {		
+		if (id == 0) {
+			return Response.serverError().entity("ID cannot be zero").build();
+		}
+
+		Course course = DAOFactory.getInstance().getCourseDao().get(id);
+		if (course.getCourseId() == -1) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+		return Response.status(Status.FOUND).entity(course).build();
 	}
 }
